ff9fd4ac894cd6184af1eb4a2ff5855305f22317
Gonzalo Salazar <gonzalo.sc2@gmail.com>
2020-08-27 18:03:16 -0400

    SEC03: way comments were made

--- Sec03_objects_and_dstructure.py
+++ Sec03_objects_and_dstructure.py
@@ -5,31 +5,31 @@ Purpose: Lecture notes
 Author : gsalazar
 """
 
-"--------------------"
-"Numbers"
+# -----------------------
+# NUMBERS
 
-'Basic Operations: sum, difference, division, multiplication'
+# Basic Operations: sum, difference, division, multiplication
 3+4
 4-3
 3/4
 4*3
 
-'Special Operations: "mod" or Modulo. It is the remainder after division.'
+# Special Operations: "mod" or Modulo. It is the remainder after division.
 7/4
 7/3
 6/6
 
-'Special Operations: power'
+# Special Operations: power
 2**3
 
-'Different ways to write 100'
+# Different ways to write 100
 50*2
 120-20+20-2*10
 (5**2)*4
 round(400/4)
 
-"--------------------"
-"Variable assignments"
+# -----------------------
+# VARIABLE ASSIGNMENTS
 
 a = 5
 a = a ** 2
@@ -43,20 +43,20 @@ my_taxes = my_income*tax_rate
 
 print(my_taxes)
 
-"--------------------"
-"Strings"
+# -----------------------
+# STRINGS
 
-'Print: one line, two lines (w & w/o indentation), tabulation'
+# Print: one line, two lines (w & w/o indentation), tabulation
 print('hello world')
 print('hello \n world')
 print('hello \nworld')
 print('hello \tworld')
 
-'Length'
+# Length
 len('hello')
 len('hello wonderful world')
 
-'Indexing starts at zero'
+# Indexing starts at zero
 char = 'hello'
 char[0]
 char[1]
@@ -64,37 +64,37 @@ char[2]
 char[3]
 char[4]
 
-'Reverse indexing also works, when we want to start from the end'
+# Reverse indexing also works, when we want to start from the end
 char[0] == char[-5]
 char[1] == char[-4]
 char[2] == char[-3]
 char[3] == char[-2]
 char[4] == char[-1]
 
-'Slicing: [start:stop:step]; step by default is 1;'
-'when using [:stop] it is to stop, but not including the stop'
-'when using [:-step] reverses the string'
+# Slicing: [start:stop:step]; step by default is 1
 char2 = 'I_like_to_code_in_Python'
 char2[-6:-1:1]
 char2[0:10:1]
 char2[0:10:2]
 char2[0:10:3]
 char2[10:]
+# when using [:stop] it is to stop, but not including the stop
 char2[:6]
+# when using [:-step] reverses the string
 char2[::2]
 char2[::4]
 char2[::-1]
 char2[::-2]
 char2[::-4]
 
-"--------------------"
-"Strings: Properties and Methods"
+# -----------------------
+# STRINGS: PROPERTIES AND METHODS
 
-'Immutability: second line (l95) will show an error'
+# Immutability: second line (l95) will show an error
 name = 'Sam'
 "name[0] = 'P'"
 
-'Concatenation'
+# Concatenation
 last_letters = name[1:]
 name1 = 'P' + last_letters
 x = 'Hello World'
@@ -102,43 +102,43 @@ x = x + ' it is beautiful outside!'
 letter = 'z'
 letter * 10
 
-'Be careful with these situations: (i) is a number, (ii) is a string'
+# Be careful with these situations: (i) is a number, (ii) is a string
 2 + 3
 '2' + '3'
 
-'Features: after definining a variable, use a dot and choose among the options'
-'For some features parenthesis should be typped in, otherwise it will display'
-'what is the command for. Using ".split" helps us to create a list with each'
-'word on a string (by default considering the blank/white space)'
+# Features: after definining a variable, use a dot and choose among the options
+# For some features parenthesis should be typped in, otherwise it will display
+# what is the command for. Using ".split" helps us to create a list with each
+# word on a string (by default considering the blank/white space)
 x.upper()
 x.lower()
 x.capitalize()
 x.split()
 x.split('o')
 
-'Interpolation: .format add the specified text inside the curly braces'
+# Interpolation: .format add the specified text inside the curly braces
 print('This is a string {}'.format('INSERTED'))
 print('The {} {} {}'.format('fox', 'brown', 'quick'))
 print('The {2} {1} {0}'.format('fox', 'brown', 'quick'))
 '           2   1   0            0       1         2'
 print('The {q} {b} {f}'.format(f='fox', b='brown', q='quick'))
 
-'Float formatting'
+# Float formatting
 result = 100/777
 print('The result was {}'.format(result))
 print('The result was {r}'.format(r=result))
-'Float formatting follows "{value:width.precision f}": width is just the'
-'length we want for the output'
+# Float formatting follows "{value:width.precision f}": width is just the
+# length we want for the output
 print('The result was {r:1.3f}'.format(r=result))
 
-'f-strings: formatted string literals. It is another way to do what is above'
+# f-strings: formatted string literals. It is another way to do what is above
 name = 'Jose'
 age = 3
 print(f'Hello, his name is {name}. His {age} years old')
 
-"--------------------"
-"Lists: Objects retrieve by locations"
-"Ordered sequence can be indexed or sliced"
+# -----------------------
+# LISTS: OBJECTS RETRIEVED BY LOCATIONS
+# Ordered sequence can be indexed or sliced
 
 my_list = ['one', 'two', 'three']
 my_list2 = ['STRING', 100, 23.4]
@@ -149,11 +149,11 @@ another_list = ['four', 'five']
 new_list = my_list + another_list
 print(new_list)
 
-'Mutation is possible here'
+# Mutation is possible here
 new_list[0] = 'ONE ALL CAP'
 print(new_list)
 
-'Add/removing items from a list. Default value for poping is "-1", the end'
+# Add/removing items from a list. Default value for poping is "-1", the end
 new_list.append('six')
 print(new_list)
 new_list.pop()
@@ -161,9 +161,10 @@ print(new_list)
 new_list.pop(3)
 print(new_list)
 
-'Sort and reverse'
+# Sort and reverse
 awesome_list = ['a', 'e', 'x', 'b', 'g']
 awesome_list_num = [1, 434, 535, 12, 43, 5, 664, 2, 43]
+sorted(awesome_list)
 print(awesome_list)
 print(awesome_list_num)
 awesome_list.sort()
@@ -175,9 +176,9 @@ awesome_list_num.reverse()
 print(awesome_list)
 print(awesome_list_num)
 
-"--------------------"
-"Dictionaries: Objects retrieved by key name"
-"Unordered and cannot be sorted"
+# -----------------------
+# DICTIONARIES: OBJECTS RETRIEVED BY KEY NAME
+# Unordered and cannot be sorted
 
 my_dict = {'key1': 'value1', 'key2': 'value2'}
 my_dict['key1']
@@ -188,7 +189,7 @@ d['k2']
 d['k2'][2]
 d['k3']['insideKey']
 
-'Similarities wih R'
+# Similarities wih R
 g = {'key1': ['a', 'b', 'c']}
 g['key1'][2].upper()
 newest_list = g['key1']
@@ -204,9 +205,8 @@ g.values()
 g.keys()
 g.items()
 
-"--------------------"
-"Tuples: Simiar to lists, but they are immutable"
-".index() returns the positions the specified value appears first"
+# -----------------------
+# TUPLES: SIMILAR TO LISTS, BUT THEY ARE IMMUTABLE
 
 t = ('a', 'a', 'b')
 my_list = [1, 2, 3]
@@ -214,10 +214,11 @@ type(t)
 type(my_list)
 
 t.count('a')
+# .index() returns the positions the specified value appears first
 t.index('a')
 
-"--------------------"
-"Sets: unordered collections of unique elements"
+# -----------------------
+# SETS: UNORDERED COLLECTION OF UNIQUE ELEMENTS
 
 myset = set()
 print(myset)
@@ -231,8 +232,8 @@ print(myset)
 mylist = [1, 1, 2, 2, 2, 2, 1, 2, 1, 2, 1, 2, 1, 3]
 set(mylist)
 
-"--------------------"
-"Booleans: operators that allow to convey T or F statements"
+# -----------------------
+# BOOLEANS: OPERATORS THAT ALLOW TO CONVEY T OR F STATEMENTS
 
 True
 type(True)
@@ -240,16 +241,16 @@ type(True)
 1 > 2
 1 == 1
 
-'Helpful when we have to define something before a loop, for example.'
-'Otherwise we might get an error'
+# Helpful when we have to define something before a loop, for example.
+# Otherwise we might get an error
 b = None
 type(b)
 
-"--------------------"
-"Files: Creating a .txt file"
+# -----------------------
+# FILES: CREATING A .TXT FILE
 
 pwd
-"cd '/Users/gsalazar/Documents/C_Codes/Python'"
+# cd '/Users/gsalazar/Documents/C_Codes/Python'
 
 %%writefile myfile.txt
 Hello this is a text file
@@ -259,36 +260,36 @@ open('myfile.txt')
 myfile = open('myfile.txt')
 open('whoops_wrong.txt')
 
-'Reads what is on the .txt file. To read it again I should reset the starting'
-'point'
+# Reads what is on the .txt file. To read it again I should reset the starting
+# point
 myfile.read()
 myfile.read()
 myfile.seek(0)
 myfile.read()
 
-'Helpful if I want to translate the file into a list'
+# Helpful if I want to translate the file into a list
 myfile.seek(0)
 myfile.readlines()
 
-'We should close it once I am done working with it'
+# We should close it once I am done working with it
 myfile.close()
 
-'We could not worry about closing a file if we "open" as follows.'
+# We could not worry about closing a file if we "open" as follows.
 with open('myfile.txt') as my_new_file:
     contents = my_new_file.read()
 
-'open() is already a built-in function. The following file has only permission'
-'to be read'
+# open() is already a built-in function. The following file has only permission
+# to be read
 with open('myfile.txt', mode='r') as myfile:
     print(myfile.read())
 
-'The following file has only permission to be written (overwrite)'
+# The following file has only permission to be written (overwrite)
 with open('myfile.txt', mode='w') as myfile:
     contents = myfile.read()
 
-"mode='a': append onlu (add on to files, lines)"
-"mode='r+': reading and writing"
-"mode='w+': writing and reading (overwrites existing file or creates one)"
+# mode='a': append onlu (add on to files, lines)
+# mode='r+': reading and writing
+# mode='w+': writing and reading (overwrites existing file or creates one)
 
 with open('myfile.txt', mode='a') as myfile:
     myfile.write('\nTHREE ON THIRD')
