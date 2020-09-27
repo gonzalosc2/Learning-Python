e097662cb6633936a0de995732ac1e069b275e4c
Gonzalo Salazar <gonzalo.sc2@gmail.com>
2020-09-27 02:48:19 -0400

    Udemy - Milestone project 1 (TIC - TAC - TOE): marginal change of file description

--- Udemy_PythonBootcamp/MilestoneProject_1/T3_functions.py
+++ Udemy_PythonBootcamp/MilestoneProject_1/T3_functions.py
@@ -2,7 +2,8 @@
 # author: Gonzalo Salazar
 # course: 2020 Complete Python Bootcamps: From Zero to Hero in Python
 # purpose: milestone project 1 - create a Tic-Tac-Toe game
-# description: main file
+# description: set of functions necessary to run this code, as well as the
+#              main body of the TIC TAC TOE game, called game()
 # other: N/A
 ####################################
 
