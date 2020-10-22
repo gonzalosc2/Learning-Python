85fe8b4a16439e3386aeb96577150e1145ecb1dd
Gonzalo Salazar <gonzalo.sc2@gmail.com>
2020-10-22 16:35:03 -0400

    udemy: pdf lecture

--- Udemy_PythonBootcamp/Sec17_PDFandCSV.py
+++ Udemy_PythonBootcamp/Sec17_PDFandCSV.py
@@ -55,3 +55,42 @@ csv_writer.writerows([['1','2','3'],['4','5','6']])
 
 data.close()
 file_to_output.close()
+
+# PDF
+# pip install PyPDF2
+import PyPDF2  # it won't work for some pdf files
+
+f = open('Working_Business_Proposal.pdf','rb')  # read binary
+pdf_reader = PyPDF2.PdfFileReader(f)
+pdf_reader.numPages
+
+page_one = pdf_reader.getPage(0)
+page_one_text = page_one.extractText()
+page_one_text   # if this is empty, it means that our pdf file is not compatible
+                # with this module
+
+# adding pages (it is not possible in the middle of a page)
+pdf_writer = PyPDF2.PdfFileWriter()
+type(page_one)
+
+pdf_writer.addPage(page_one)
+
+pdf_output = open('Some_BrandNew_Doc.pdf','wb')
+
+pdf_writer.write(pdf_output)
+
+f.close()
+pdf_output.close()
+
+# grabbing text
+f = open('Working_Business_Proposal.pdf','rb')
+
+pdf_text = []
+pdf_reader = PyPDF2.PdfFileReader(f)
+
+for num in range(pdf_reader.numPages):
+
+    page = pdf_reader.getPage(num)
+    pdf_text.append(page.extractText())
+
+pdf_text
