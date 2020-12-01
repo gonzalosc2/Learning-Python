0c3d4bc3e04fdfdb24b80f076cfd9a8da89f1f8e
Gonzalo Salazar <gonzalo.sc2@gmail.com>
2020-12-01 19:14:19 -0300

    Udemy: Plotly and Cufflinks. Awesome library!

--- Udemy_Py_DataScience_ML/Sec10_dataVisual_Pandas_built-in_DV.py
+++ Udemy_Py_DataScience_ML/Sec10_dataVisual_Pandas_built-in_DV.py
@@ -7,3 +7,38 @@
 ####################################
 
 import pandas as pd
+import seaborn as sns
+import numpy as np
+import os
+
+os.getcwd()
+os.chdir('/Users/gsalazar/Documents/C_Codes/Learning-Python/Udemy_Py_DataScience_ML/Pandas_examples')
+
+df1 = pd.read_csv('df1',index_col = 0)
+df1.head()
+df2 = pd.read_csv('df2')
+df2.head()
+
+## Plotting
+df1['A'].hist(bins=30)
+df1['A'].plot(kind = 'hist',bins = 30)
+df1['A'].plot.hist(bins = 30)
+
+df2.plot.area(alpha = 0.4)
+df2.plot.bar(alpha = .5,stacked = True)
+
+#df1.plot.line(x=df1.index,y='B')  # it returns me an error
+df1['A'].plot.line(alpha=.5,figsize = (12,3), lw = 1)
+
+df1.plot.scatter(x='A',y='B')
+df1.plot.scatter(x='A',y='B',c='C',cmap = 'coolwarm')
+df1.plot.scatter(x='A',y='B',s=df1['C']*20)
+
+df2.plot.box()
+
+df = pd.DataFrame(np.random.randn(1000,2),columns=['A','B'])
+df.plot.hexbin(x='A',y='B',gridsize = 30)
+
+df2['a'].plot.kde()
+df2['a'].plot.density()  # the same as the previos command
+df2.plot.density()
--- Udemy_Py_DataScience_ML/Sec11_dataVisual_Plotly_Cufflinks.py
+++ Udemy_Py_DataScience_ML/Sec11_dataVisual_Plotly_Cufflinks.py
@@ -6,4 +6,65 @@
 # other: N/A
 ####################################
 
+# Due to an update, I should be considering the following:
+#
+# 1. Possible Import Error 1: You need to install a new package. In your command
+#line type and run:
+#   pip install chart-studio
+#
+# Then in jupyter make sure you import it by running the code:
+#    import chart_studio.plotly as py
+#
+# 2.Possible Colorscale Error 2: In the "Real Data US Map Choropleth", when
+# you are creating the data dictionary, make sure the colorscale line
+# is = 'ylorbr', not 'YIOrbr'... so like this:
+#    colorscale='ylorbr'
+#
+# 3.Possible projection Error 3: In the "World Map Choropleth", when you are
+# creating the layout, ensure that your projection line
+# is = {'type':'mercator'} not Mercator with a capital...so like this:
+#   projection={'type':'mercator'}
+
+#conda install plotly
+#pip install cufflinks  # connects ployly with Pandas
+
+# Plotly and Cufflinks
 import pandas as pd
+import numpy as np
+#from plotly import __version__
+#print(__version__)
+import cufflinks as cf
+from plotly.offline import download_plotlyjs, init_notebook_mode,plot,iplot
+
+# Connecting JavaScript (interactive library) to a Python script
+init_notebook_mode(connected=True)
+# Making offline use of cufflinks
+cf.go_offline()
+# DATA
+df = pd.DataFrame(np.random.randn(100,4),columns = 'A B C D'.split())
+df.head()
+
+df2 = pd.DataFrame({'Category':['A','B','C'],'Values':[32,43,50]})
+df2
+
+## Interactive plots!!
+df.iplot()
+
+df.iplot(kind = 'scatter',x='A',y='B',mode = 'markers')  # size option also available
+df2.iplot(kind = 'bar',x='Category',y='Values')
+
+# aggregation or groupby method
+df.sum().iplot(kind = 'bar',mode = 'markers')
+
+df.iplot(kind = 'box')
+df['A'].iplot(kind = 'hist',bins = 40)
+df.iplot(kind = 'hist',bins = 40)  # overlapping histograms for comparison
+df.iplot(kind='bubble',x='A',y='B',size='C')
+df.scatter_matrix()  # similar to a pairplot
+
+# 3D surface plot
+df3 = pd.DataFrame({'x':[1,2,3,4,5],'y':[10,20,30,20,10],'z':[5,4,3,2,1]})
+df3.iplot(kind = 'surface',colorscale='rdylbu')
+
+# financial data visualization
+df[['A','B']].iplot(kind = 'spread')
