6d80c7889c132eb002eaf6228c2ac99952c7a459
Gonzalo Salazar <gonzalo.sc2@gmail.com>
2020-12-01 18:40:10 -0300

    Udemy: Pandas built-in data visualization + minor mod [names, confusion]

---
+++ Udemy_Py_DataScience_ML/Sec10_dataVisual_Pandas_built-in_DV.py
@@ -0,0 +1,9 @@
+####################################
+# author: Gonzalo Salazar
+# course: Python for Data Science and Machine Learning Bootcamp
+# purpose: lecture notes
+# description: Section 10 - Python for Data Visualization, Pandas Built-in DV
+# other: N/A
+####################################
+
+import pandas as pd
---
+++ Udemy_Py_DataScience_ML/Sec11_dataVisual_Plotly_Cufflinks.py
@@ -0,0 +1,9 @@
+####################################
+# author: Gonzalo Salazar
+# course: Python for Data Science and Machine Learning Bootcamp
+# purpose: lecture notes
+# description: Section 11 - Python for Data Visualization, Plotly and Cufflinks
+# other: N/A
+####################################
+
+import pandas as pd
