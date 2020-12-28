4e04262e805934a39425153f0f23958a378dff3a
Gonzalo Salazar <gonzalo.sc2@gmail.com>
2020-12-28 15:20:16 -0300

    lr - lectures (training)

---
+++ Udemy_Py_DataScience_ML/Sec15_LinearReg.py
@@ -0,0 +1,59 @@
+####################################
+# author: Gonzalo Salazar
+# course: Python for Data Science and Machine Learning Bootcamp
+# purpose: lecture notes
+# description: Section 15 - Linear Regression
+# other: N/A
+####################################
+
+#%%
+import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+#%matplotlib inline
+os.chdir('/Users/gsalazar/Documents/C_Codes/Learning-Python/Udemy_Py_DataScience_ML/lr_data')
+
+#%%
+df = pd.read_csv('USA_Housing.csv')
+
+# %%
+# Brief descriptive statistics from the data
+df.head()
+df.info()
+df.describe()
+
+# %%
+# Describing the data we'll work with
+sns.pairplot(df)
+sns.displot(df['Price'],kde = True)
+sns.heatmap(df.corr(),cmap = 'Greens')
+plt.show()
+
+# %%
+# Establishing the data we'll work with as well as defining training and testing samples
+X = df[df.columns[:5]]
+y = df['Price']
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size = 0.4, random_state=101)
+
+## TRAINING/ESTIMATION
+# %%
+# Instantiating a linear regression model and training it
+lm = LinearRegression()  
+lm.fit(X_train,y_train)
+
+# %%
+# Estimated coefficients 
+cdf = pd.DataFrame(lm.coef_,X.columns, columns = ['Coeff'])
+cdf
+
+# Interpretation (example)
+# holding everything else constant, a one unit increase in average area income
+# is associated with a 21.52 dolars increase in the unit price
+
+## PREDICTION
+
