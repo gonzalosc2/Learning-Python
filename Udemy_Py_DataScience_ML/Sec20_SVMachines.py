a39d3f24e674b09ccaa76e34c2981c293b313736
Gonzalo Salazar <gonzalo.sc2@gmail.com>
2020-12-31 15:49:29 -0300

    SVM - project: completed

--- Udemy_Py_DataScience_ML/Sec20_SVMachines.py
+++ Udemy_Py_DataScience_ML/Sec20_SVMachines.py
@@ -72,46 +72,132 @@ print(classification_report(y_test,pred))
 print('\nConfusion Matrix')
 print(confusion_matrix(y_test,pred))
 
-# NOTICE: if we are classifying everything into a single class, this means our
-#         model needs to have it parameters adjusted (it may also help to 
-#         normalize the data).
+# NOTICE: if we are classifying everything into a single class, this means our model needs to have
+#         it parameters adjusted (it may also help to normalize the data).
 #         
-#         We do so by using a GridSearch. Finding the right parameters (like
-#         what C or gamma values to use) is a tricky task! But luckily, we 
-#         can be a little lazy and just try a bunch of combinations and see what 
-#         works best! This idea of creating a 'grid' of parameters and just trying 
-#         out all the possible combinations is called a Gridsearch, this method
-#         is common enough that Scikit-learn has this functionality built in with
-#         GridSearchCV! The CV stands for cross-validation which is the 
-#         GridSearchCV takes a dictionary that describes the parameters that
-#         should be tried and a model to train. The grid of parameters is defined 
-#         as a dictionary, where the keys are the parameters and the values are 
-#         the settings to be tested.   
+#         We do so by using a GridSearch. Finding the right parameters (like what C or gamma 
+#         values to use) is a tricky task! But luckily, we can be a little lazy and just try a
+#         bunch of combinations and see what works best! 
+# 
+#         The idea of creating a 'grid' of parameters and just trying out all the possible 
+#         combinations is called a Gridsearch, this method is common enough that Scikit-learn 
+#         has this functionality built in with GridSearchCV! 
+# 
+#         The CV stands for cross-validation which is the GridSearchCV takes a dictionary that
+#         describes the parameters that should be tried and a model to train. The grid of 
+#         parameters is defined as a dictionary, where the keys are the parameters and the 
+#         values are the settings to be tested.   
+
+# In SVM, C controls the cost of misclassification on the training data. A large C value gives us 
+# low bias (we penalize more the cost of misclassification) and high variance, and vice versa.
+# The gamma parameter has to do with the free parameter of the Gaussian radial basis function
+# (which is the default kernel used in SVC). A small gamma means a Gaussian for large variance, where
+# a large gamma value is going to lead to a high bias and low variance in the model (hence the 
+# support vector does not have a widespread influence).
 
 # %%
-del y, X, X_test, X_train, y_train, y_test, dtree, pred, df, rf, rf_pred
+# GRID SEARCH (can take a very long time. Depends on the number of parameters to test and # obs)
+param_grid = {'C':[0.1,1,10,100,1000],'gamma':[1,0.1,0.01,0.001,0.0001]}
+grid = GridSearchCV(SVC(),param_grid,verbose = 3)  # the higher the number, the more description
+                                                   # in the ooutput
+grid.fit(X_train,y_train)
 
+# %%
+# Getting the parameters after CV
+grid.best_params_
+grid.best_estimator_
+
+# %%
+# Predicting points classification
+grid_pred = grid.predict(X_test)
+
+# Checking performance
+print('Classification Report')
+print(classification_report(y_test,grid_pred))
+print('\nConfusion Matrix')
+print(confusion_matrix(y_test,grid_pred))
+
+# %%
+del y, X, X_test, X_train, y_train, y_test, grid, pred, grid_pred, param_grid, svm
+
+# %%
 ####################################################################################
-# PROJECT EXERCISE - Loan Data
+# PROJECT EXERCISE - Iris
 
-# GOAL: trying to classify and predict whether or not the borrower paid back their loan in full.
-# 
+from sklearn.datasets import load_iris
+iris = load_iris()
+target = pd.DataFrame(iris['target'],columns=['species'])
+data = pd.DataFrame(iris['data'],columns=iris['feature_names'])
+iris = pd.concat([target,data],axis=1)
+iris.head()
 
-# Here are what the columns represent:
+# Iris plants dataset
 
-    # credit.policy: 1 if the customer meets the credit underwriting criteria of LendingClub.com, and 0 otherwise.
-    # purpose: The purpose of the loan (takes values "credit_card", "debt_consolidation", "educational",
-    #          "major_purchase", "small_business", and "all_other").
-    # int.rate: The interest rate of the loan, as a proportion (a rate of 11% would be stored as 0.11). Borrowers 
-    #           judged by LendingClub.com to be more risky are assigned higher interest rates.
-    # installment: The monthly installments owed by the borrower if the loan is funded.
-    # log.annual.inc: The natural log of the self-reported annual income of the borrower.
-    # dti: The debt-to-income ratio of the borrower (amount of debt divided by annual income).
-    # fico: The FICO credit score of the borrower.
-    # days.with.cr.line: The number of days the borrower has had a credit line.
-    # revol.bal: The borrower's revolving balance (amount unpaid at the end of the credit card billing cycle).
-    # revol.util: The borrower's revolving line utilization rate (the amount of the credit line used relative to 
-    #             total credit available).
-    # inq.last.6mths: The borrower's number of inquiries by creditors in the last 6 months.
-    # delinq.2yrs: The number of times the borrower had been 30+ days past due on a payment in the past 2 years.
-    # pub.rec: The borrower's number of derogatory public records (bankruptcy filings, tax liens, or judgments).
+#     :Number of Instances: 150 (50 in each of three classes)
+#     :Number of Attributes: 4 numeric, predictive attributes and the class
+#     :Attribute Information:
+#         - sepal length in cm
+#         - sepal width in cm
+#         - petal length in cm
+#         - petal width in cm
+#         - class:
+#                 - Iris-Setosa
+#                 - Iris-Versicolour
+#                 - Iris-Virginica
+
+# %%
+# Exploratory Data Analysis
+# Creatin a pairplot of the data set.
+sns.pairplot(iris, corner=True, hue='species')
+# Comment: Note setosa seems to be the most separable species compared to versicolour and virginica.
+#          It does not share all the same characteristics as the other two.
+
+# %%
+# Creating a kde plot of sepal_length versus sepal width for setosa species of flower
+sns.set_style('darkgrid')
+sns.kdeplot(y='sepal length (cm)',x='sepal width (cm)',data = iris[iris['species']==1],fill=True,cmap='Greens')
+
+# %%
+# Splitting data by test and train data
+X = iris.drop('species',axis=1)
+y = iris['species']
+
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size = 0.3)
+
+# %%
+# SVM
+# Instantiating the model and training it
+svm = SVC()
+svm.fit(X_train,y_train)
+
+# %%
+# Predicting points classification
+pred = svm.predict(X_test)
+
+# %%
+# Checking performance
+print('Classification Report')
+print(classification_report(y_test,pred))
+print('\nConfusion Matrix')
+print(confusion_matrix(y_test,pred))
+
+# %%
+# GRID SEARCH 
+param_grid = {'C':[0.1,1,10,100,1000],'gamma':[1,0.1,0.01,0.001,0.0001]}
+grid = GridSearchCV(SVC(),param_grid,verbose = 3) 
+grid.fit(X_train,y_train)
+
+# %%
+# Getting the parameters after CV
+grid.best_params_
+grid.best_estimator_
+
+# %%
+# Predicting points classification
+grid_pred = grid.predict(X_test)
+
+# Checking performance
+print('Classification Report')
+print(classification_report(y_test,grid_pred))
+print('\nConfusion Matrix')
+print(confusion_matrix(y_test,grid_pred))
