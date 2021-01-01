588cf5132e650f2b6149c953e9e754e281bbf70b
Gonzalo Salazar <gonzalo.sc2@gmail.com>
2021-01-01 17:11:37 -0300

    nlp - lecture

--- Udemy_Py_DataScience_ML/Sec23_RecommenderSys.py
+++ Udemy_Py_DataScience_ML/Sec23_RecommenderSys.py
@@ -37,7 +37,7 @@ import seaborn as sns
 os.chdir('/Users/gsalazar/Documents/C_Codes/Learning-Python/Udemy_Py_DataScience_ML/rsys_data')
 # %%
 # Loading movie data
-columns_names = ['used_id','item_id','rating','timestamp']
+columns_names = ['user_id','item_id','rating','timestamp']
 df = pd.read_csv('u.data',sep='\t',names=columns_names)
 df.head()
 
@@ -79,3 +79,51 @@ ratings['rating'].hist(bins=70)
 # Checking the relationship between both
 sns.jointplot(x='rating', y='num of ratings',data=ratings,alpha=.5)
 # Comment: the higher the number of reviews, the higher the rating of a movie
+
+# %%
+# Creating a matrix of user_id vs. movies where values = rating
+moviemat = df.pivot_table(index = 'user_id',columns='title',values='rating')
+moviemat.head()
+# Comment: there are plenty of missing values since not everyone has seen all movies
+
+# %%
+# Checking out the most rated movies again and choosing two movies and grabbing their
+# user ratings
+ratings.sort_values('num of ratings', ascending = False).head(10)
+starwars_user_ratings = moviemat['Star Wars (1977)']
+liarliar_user_ratings = moviemat['Liar Liar (1997)']
+# Comment: Choosing Star Wars and Liar Liar
+
+# %%
+# Computing the pairwise correlation between rows or columns of two DataFrame objects
+# (instead of just the index or columns of a DataFrame)
+similar_to_starwars = moviemat.corrwith(starwars_user_ratings)
+similar_to_liarliar = moviemat.corrwith(liarliar_user_ratings)
+
+corr_starwars = pd.DataFrame(similar_to_starwars,columns=['Correlation'])
+corr_starwars.dropna(inplace=True)
+corr_starwars.head()
+# Comment: We get a bunch of titles of movies adn the correlation column, which tells us 
+#          how correlated this movies user ratings were to the user rating of the Star 
+#          Wars movie.
+
+# %%
+# Sorting values out
+corr_starwars.sort_values('Correlation',ascending=False).head(10)
+#Comment: Notice that some correlations do not make sense because most likely these
+#         movies happened to have been seen only by one person who also happened to
+#         rate Star Wars five stars. We can fix this by filtering movies that have
+#         under a certain number of reviews.
+
+# %%
+# Checking the histogram of movies by number of ratings, we see that after a 100 
+# reviews the histogram seems to 'normalize' or decline seriously.
+corr_starwars = corr_starwars.join(ratings['num of ratings'])
+corr_starwars[corr_starwars['num of ratings']>100].sort_values('Correlation',ascending=False).head(10)
+
+# %%
+# Checking for Liar Liar
+corr_liarliar = pd.DataFrame(similar_to_liarliar,columns=['Correlation'])
+corr_liarliar.dropna(inplace=True)
+corr_liarliar = corr_liarliar.join(ratings['num of ratings'])
+corr_liarliar[corr_liarliar['num of ratings']>100].sort_values('Correlation',ascending=False).head(10)
